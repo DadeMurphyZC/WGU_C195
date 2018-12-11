b4bae92b135c0dbb23d8fb3d06b75b40eb1850b1
Cristoval Fonseca <cfonseca@protonmail.com>
2018-12-10 16:30:38 -0800

    Created TableRow class for Customer FXML mapping

---
+++ src/c195_schedulingapp/utils/TableRow.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c195_schedulingapp.utils;
+
+import javafx.beans.value.ObservableValue;
+
+/**
+ *
+ * @author cfonseca
+ */
+public class TableRow {
+    private ObservableValue<String> customerName;
+    private ObservableValue<String> address;
+    private ObservableValue<String> phone;
+    
+    public TableRow(ObservableValue<String> customerName, ObservableValue<String> address, ObservableValue<String> phone) {
+        this.customerName = customerName;
+        this.address = address;
+        this.phone = phone;
+    }
+    
+    public ObservableValue<String> getcustomerName() {
+        return customerName;
+    }
+
+    public void setcustomerName(ObservableValue<String> customerName) {
+        this.customerName = customerName;
+    }
+
+    public ObservableValue<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(ObservableValue<String> address) {
+        this.address = address;
+    }
+
+    public ObservableValue<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(ObservableValue<String> phone) {
+        this.phone = phone;
+    }
+}
