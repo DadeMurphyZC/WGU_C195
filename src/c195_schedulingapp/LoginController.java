9b441394644d8e652893d657851b5dfe114d118b
Cristoval Fonseca <cfonseca@protonmail.com>
2019-01-02 10:17:56 -0800

    Test: added location combobox options and set state.setLocation() after login on the Customers init method

--- src/c195_schedulingapp/LoginController.java
+++ src/c195_schedulingapp/LoginController.java
@@ -5,21 +5,18 @@
  */
 package c195_schedulingapp;
 
-import static c195_schedulingapp.C195_SchedulingApp.state;
 import static c195_schedulingapp.C195_SchedulingApp.appStage;
+import static c195_schedulingapp.C195_SchedulingApp.state;
 import c195_schedulingapp.utils.DB;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
 
 /**
  * FXML Controller class
@@ -31,9 +28,11 @@ public class LoginController implements Initializable {
     @FXML private TextField username;
     @FXML private PasswordField password;
     @FXML private Button loginBtn;
+    @FXML private ComboBox location;
     
     @FXML public void login() throws ClassNotFoundException, SQLException, IOException{
         if(DB.loginAuth(username.getText(), password.getText())==true){
+            state.setLocation(location.getSelectionModel().getSelectedItem().toString());
             Parent root = FXMLLoader.load(getClass().getResource("Customers.fxml"));
             Scene scene = new Scene(root);
             appStage.setScene(scene);
@@ -45,9 +44,16 @@ public class LoginController implements Initializable {
     
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        if(!location.getItems().isEmpty()){location.getItems().clear();}
+        location.getItems().add("America/New_York");
+        location.getItems().add("America/Phoenix");
+        location.getItems().add("Europe/London");
+        location.getItems().add("Online");
     }    
     
 }
