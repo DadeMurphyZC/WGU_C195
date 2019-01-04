3aec156a8b9e0632f5ded2b60c7446ca0956ec64
Cristoval Fonseca <cfonseca@protonmail.com>
2019-01-04 12:05:39 -0800

    Added FXML button and combobox Added populateReports() method to add combobox choices at init Added reportsTest() method to test fxml button binding

--- src/c195_schedulingapp/ReportsController.java
+++ src/c195_schedulingapp/ReportsController.java
@@ -6,8 +6,12 @@
 package c195_schedulingapp;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 /**
  * FXML Controller class
@@ -15,13 +19,31 @@ import javafx.fxml.Initializable;
  * @author cfonseca
  */
 public class ReportsController implements Initializable {
-
+    
+    @FXML private Button generateBtn;
+    @FXML private ComboBox reportsCB;
+    
+    final ArrayList reports = new ArrayList();
+    
+    public void populateReports(){
+        reports.add("Report 1");
+        reports.add("Report 2");
+        reports.add("Report 3");
+        reportsCB.getItems().addAll(reports);
+    }
+    
+    public void reportsTest(){
+        System.out.println("Running report: "+reportsCB.getSelectionModel().getSelectedItem().toString());
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        if(reportsCB.getItems().isEmpty()){} else {
+            reportsCB.getItems().clear();
+        };
+        populateReports();
     }    
     
 }
