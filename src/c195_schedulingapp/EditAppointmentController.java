9bbcfe3f0b361eaeb549eb85f11858f845fb2ac9
DadeMurphyZC <cfonseca@protonmail.com>
2019-01-07 14:28:28 -0800

    added EditAppointment controller and initial FXML gui objects

--- src/c195_schedulingapp/EditAppointmentController.java
+++ src/c195_schedulingapp/EditAppointmentController.java
@@ -41,7 +41,6 @@ public class EditAppointmentController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }    
     
 }
