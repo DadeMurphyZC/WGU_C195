de48640f902c5907f5311008b6cfa2e757684a51
DadeMurphyZC <cfonseca@protonmail.com>
2019-01-07 12:05:04 -0800

    created EditAppointment controller added FXML gui elements

---
+++ src/c195_schedulingapp/EditAppointmentController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c195_schedulingapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author cfonseca
+ */
+public class EditAppointmentController implements Initializable {
+    
+    @FXML private ComboBox contact;
+    @FXML private ComboBox customer;
+    @FXML private ComboBox description;
+    @FXML private ComboBox location;
+    @FXML private ComboBox startTime;
+    @FXML private ComboBox endTime;
+    @FXML private TextField title;
+    @FXML private DatePicker date;
+    @FXML private Button save;
+    @FXML private Button cancel;
+    
+    public void populateForm(){
+        
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
