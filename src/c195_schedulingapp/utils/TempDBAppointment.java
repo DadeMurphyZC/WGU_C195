69acc0e9fa552f123e5904fd90f1e762c007f6d7
DadeMurphyZC <cfonseca@protonmail.com>
2019-01-07 14:29:49 -0800

    created TempDBAppointment with default constructor to help build a row object to add

---
+++ src/c195_schedulingapp/utils/TempDBAppointment.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c195_schedulingapp.utils;
+
+import c195_schedulingapp.Model.Appointment;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author cfonseca
+ */
+public class TempDBAppointment {
+    public static Appointment tempAppointment(ResultSet rs) throws SQLException{
+        Appointment temp = new Appointment();
+                temp.setAppointmentId(rs.getInt("appointmentid"));
+                temp.setTitle(rs.getString("title"));
+                temp.setDescription(rs.getString("description"));
+                temp.setLocation(rs.getString("location"));
+                temp.setContact(rs.getString("contact"));
+                temp.setUrl(rs.getString("url"));
+                temp.setStart(rs.getString("start"));
+                temp.setEnd(rs.getString("end"));
+        return temp;
+    }
+    
+    //TEMP DESTRUCTOR, SETS IT TO NULL FOR GARBAGE COLLECTION
+    public static void closeAppointment(Appointment a){
+        a = null;
+    }
+}
