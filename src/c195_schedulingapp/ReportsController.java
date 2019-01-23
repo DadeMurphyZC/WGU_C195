804cdb3404cf436e8c9eb3a90e357c9ec8bd561b
Cristoval Fonseca <cfonseca@protonmail.com>
2019-01-23 14:00:44 -0800

    added scene change methods

--- src/c195_schedulingapp/ReportsController.java
+++ src/c195_schedulingapp/ReportsController.java
@@ -5,10 +5,12 @@
  */
 package c195_schedulingapp;
 
+import static c195_schedulingapp.C195_SchedulingApp.appStage;
 import static c195_schedulingapp.utils.DB.getApptTypesByMonth;
 import static c195_schedulingapp.utils.DB.getApptsByLocation;
 import static c195_schedulingapp.utils.DB.getApptsByUser;
 import c195_schedulingapp.utils.ReportRow;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,7 +23,10 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
@@ -41,7 +46,30 @@ public class ReportsController implements Initializable {
     
     public static ObservableList<ReportRow> reportsList = FXCollections.observableArrayList();
     
+    @FXML
+    public void openAppointments() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Appointments.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
+    
+    @FXML
+    public void openCustomers() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Customers.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
 
+    @FXML
+    public void openCalendar() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
+    
     final HashMap reportshm = new HashMap();
     public static ReportRow selected = new ReportRow();
     
