8a2ea406eaecd3b1d0432828443f412db88dfbd0
Cristoval Fonseca <cfonseca@protonmail.com>
2019-01-23 14:00:37 -0800

    added scene change methods

--- src/c195_schedulingapp/CalendarController.java
+++ src/c195_schedulingapp/CalendarController.java
@@ -5,10 +5,15 @@
  */
 package c195_schedulingapp;
 
+import static c195_schedulingapp.C195_SchedulingApp.appStage;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
@@ -20,37 +25,65 @@ import javafx.scene.control.TableView;
  * @author cfonseca
  */
 public class CalendarController implements Initializable {
-    
-    @FXML private ComboBox months;
-    @FXML private Button getMonthlyBtn;
-    @FXML private Button getWeeklyBtn;
-    @FXML private TableView calendarTable;
-    @FXML private TableColumn calendarCol;
-    
-    private void setMonths(){
-        months.getItems().clear();
-        months.getItems().setAll
-           ("January", 
-            "February", 
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "Obtober",
-            "November",
-            "December"
-           );
+
+    @FXML
+    private ComboBox months;
+    @FXML
+    private Button getMonthlyBtn;
+    @FXML
+    private Button getWeeklyBtn;
+    @FXML
+    private TableView calendarTable;
+    @FXML
+    private TableColumn calendarCol;
+
+    @FXML
+    public void openAppointments() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Appointments.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
     }
-    
+
+    @FXML
+    public void openReports() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Reports.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
+        
+    @FXML
+    public void openCustomers() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Customers.fxml"));
+        Scene scene = new Scene(root);
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    private void setMonths() {
+        months.getItems().clear();
+        months.getItems().setAll("January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "Obtober",
+                "November",
+                "December"
+        );
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setMonths();
-    }    
-    
+    }
+
 }
